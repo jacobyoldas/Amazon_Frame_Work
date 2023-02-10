a4be691c2dfef3c59f22f253234f94800ceec2cb
jacobyoldas <yasinyoldas6666@gmail.com>
2023-02-10 02:08:06 -0500

    updated

--- src/Amazon/Amazon_Product_Detail_Page.java
+++ src/Amazon/Amazon_Product_Detail_Page.java
@@ -3,14 +3,9 @@ package Amazon;
 public class Amazon_Product_Detail_Page {
     public static void main(String[] args) {
         detailOfProduct();
-        detailOfProduct();
-
-
     }
     public static void detailOfProduct(){
         String product="Peace";
         System.out.println(product);
-        System.out.println(product);
-
     }
 }
