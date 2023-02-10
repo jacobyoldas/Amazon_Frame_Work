afd8e07294d31736c9debb2c0d7f66db6576b4e3
jacobyoldas <yasinyoldas6666@gmail.com>
2023-02-10 09:06:55 -0500

    updated

--- src/Amazon/Login.java
+++ src/Amazon/Login.java
@@ -6,7 +6,12 @@ public class Login {
     System.out.println("Done!");
     System.out.println("111111111111111111");
     System.out.println("333333333333333333");
+    System.out.println("##############");
     System.out.println("4444");
+    System.out.println("4444");
+    System.out.println("$$$$$$$$$$$$$$$$");
+    System.out.println("%%%%%%%%%%%%%%%%");
+
 
   }
 
