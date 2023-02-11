4757ce6ec972b9a85ada4b379c0d04fc3feb84fa
jacobyoldas <yasinyoldas6666@gmail.com>
2023-02-11 03:15:50 -0500

    code added

--- src/Amazon/Login.java
+++ src/Amazon/Login.java
@@ -3,14 +3,14 @@ package Amazon;
 public class Login {
 
   /**
-   * When/How/Why you get conflict? (whichever class committed and pushed)
-   * How to handle conflict? (work around, show diff)
+   * When/How/Why you get conflict? (whichever class committed-pushed amd you work on that class without update/MERGE)
+   * How to handle conflict? (work around, using guidance{symbols},show diff, unique{owm} class)
    * IDU? (practice and play around, ask yourself q/a)
    */
 
   public static void main(String[] args) {
     System.out.println("Hello, World!");
-    System.out.println("Hello, World!");
-     }
+
+  }
 
 }
